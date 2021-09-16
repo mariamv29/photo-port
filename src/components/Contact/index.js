ff2149fb0b3e17c9dbd38dcb85da0e97592825a1
Maria Mireles <mireles335@gmail.com>
2021-09-15 22:55:52 -0500

    added state/unsure it works moduleunclear

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const { name, email, message } = formState;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+    
+  } 
+  function handleChange(e) {
+    setFormState({ ...formState, name: e.target.value });
+  }
+
+  console.log(formState);
+
   return (
     <section>
       <h1>Contact Me</h1>
-      <form id="contact-form">
-        <div>
+      <form id="contact-form" onSubmit={handleSubmit}>
+      <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
         </div>
-        <button type="submit">Submit</button>
+
+        <button data-testid="button" type="submit">
+          Submit
+        </button>
       </form>
     </section>
   );
